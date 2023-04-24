87a7bb530c4f371880f5011b05e616a61f5c3301
Rodney-D-Dev <>
2023-04-24 14:47:53 +0000

    Add game code for division

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1,num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1,num2);
-    }else if (gameType === "subtract" ) {
+    } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1,num2);
+    } else if (gameType === "division" ) {
+        dispalyDivisionQuestion(num1,num2);
     } else {
         AudioWorklet(`Unkown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -90,6 +92,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -134,4 +138,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "X" ;
 
+}
+
+function dispalyDivisionQuestion(operand1, operand2) {
+    
+    operand1 = operand1 * operand2;
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/" ;
 }
